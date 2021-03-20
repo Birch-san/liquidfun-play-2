263fc0c63ca33effec23aa032df3474861ae46e1
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-20 19:44:59 +0000

    lint script

--- src/static.d.ts
+++ src/static.d.ts
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="@types/snowpack-env" />
 declare module '*.module.css' {
-  const classes: {[key: string]: string};
-  export default classes;
+  const classes: {[key: string]: string}
+  export default classes
 }
