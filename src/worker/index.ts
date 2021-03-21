6d7866d55931d7baad4b3ef06a5a7ffe5f193a44
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-21 16:13:31 +0000

    empty shaders

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -5,6 +5,12 @@ self.onmessageerror = (event: MessageEvent) =>
 self.onerror = (event: ErrorEvent) =>
   console.error('onerror', event)
 
+const vertexShaderResponse: Response = await fetch(new URL('../../shader.vert', import.meta.url).toString())
+const vertexShaderText: string = await vertexShaderResponse.text()
+
+const fragmentShaderResponse: Response = await fetch(new URL('../../shader.frag', import.meta.url).toString())
+const fragmentShaderText: string = await fragmentShaderResponse.text()
+
 const { box2D } = await import('./box2d')
 const { debugDraw } = await import('./debugDraw')
 
@@ -49,10 +55,59 @@ setInterval(() => {
   renderedThisFrame = false
 }, physicsIntervalMs)
 
-const onContext = (ctx: WebGL2RenderingContext): void => {
+const onContext = (ctx: WebGLRenderingContext): void => {
+  const compile = (type: GLenum, shaderStr: string): WebGLShader => {
+    const shader: WebGLShader | null = ctx.createShader(type)
+    if (shader === null) {
+      throw new Error('Failed to create WebGLShader')
+    }
+    ctx.shaderSource(shader, shaderStr)
+    ctx.compileShader(shader)
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!ctx.getShaderParameter(shader, ctx.COMPILE_STATUS)) {
+      const error: string | null = ctx.getShaderInfoLog(shader)
+      throw new Error(`Shader compilation failed${error == null ? '' : `: ${error}`}`)
+    }
+    return shader
+  }
+  const link = (shaders: WebGLShader[]): WebGLProgram => {
+    const program: WebGLProgram | null = ctx.createProgram()
+    if (program === null) {
+      throw new Error('Failed to create WebGLProgram')
+    }
+    for (const shader of shaders) {
+      ctx.attachShader(program, shader)
+    }
+    ctx.linkProgram(program)
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!ctx.getProgramParameter(program, ctx.LINK_STATUS)) {
+      const error: string | null = ctx.getProgramInfoLog(program)
+      throw new Error(`WebGL program link failed${error == null ? '' : `: ${error}`}`)
+    }
+
+    return program
+  }
+
+  const vertexShader: WebGLShader = compile(WebGLRenderingContext.VERTEX_SHADER, vertexShaderText)
+  const fragmentShader: WebGLShader = compile(WebGLRenderingContext.FRAGMENT_SHADER, fragmentShaderText)
+
+  const program: WebGLProgram = link([vertexShader, fragmentShader])
+
+  ctx.useProgram(program)
+
+  const draw = (): void => {
+    // const n = 0
+    ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT)
+
+    world.DebugDraw()
+    // ctx.drawArrays(ctx.TRIANGLES, 0, n)
+  }
+
   const render: FrameRequestCallback = (): void => {
     if (!renderedThisFrame) {
-      world.DebugDraw()
+      draw()
       renderedThisFrame = true
     }
     requestAnimationFrame(render)
@@ -62,7 +117,7 @@ const onContext = (ctx: WebGL2RenderingContext): void => {
 
 self.onmessage = ({ data }: MessageEvent<FromMain>) => {
   if (data.type === 'offscreenCanvas') {
-    const ctx: WebGL2RenderingContext | null = data.offscreenCanvas.getContext('webgl2')
+    const ctx: WebGLRenderingContext | null = data.offscreenCanvas.getContext('webgl')
     if (ctx === null) {
       throw new Error('Failed to create WebGL2 rendering context')
     }
