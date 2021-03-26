978fa4973e8871cb3c669f23b8725d8358c0c8f4
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-26 23:03:50 +0000

    working but small

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -44,7 +44,7 @@ const switchDemo = async (proposedDemo: Demo): Promise<void> => {
   }
 }
 
-const frameLimit = 30
+const frameLimit = 90
 const minimumWaitMs = 1 / frameLimit * 1000
 const shouldRun: ShouldRun = (intervalMs: number): boolean =>
   intervalMs > minimumWaitMs && world !== undefined
