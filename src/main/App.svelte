46e1fc97df20374a733ea8c20826fdb42b39f61f
Alex Birch <Birch-san@users.noreply.github.com>
2021-04-02 18:43:34 +0100

    links

--- src/main/App.svelte
+++ src/main/App.svelte
@@ -88,6 +88,8 @@
     <pre class="fatal-error">{fatalError}</pre>
     <h3>Sorry, a fatal error occurred.</h3>
     <p>This experiment relies on a lot of new Web Worker functionality (<a href="https://stackoverflow.com/a/45578811/5257399">ES imports</a> and <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers"><code>requestAnimationFrame</code></a>). As such, it is currently only expected to work in Chrome/Chromium-based browsers. The plan for <a href="https://github.com/Birch-san/box2d-wasm/discussions/24#discussioncomment-540893">Firefox support</a> will be to use <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer"><code>SharedArrayBuffer</code></a>. The plan for <a href="https://caniuse.com/?search=SharedArrayBuffer">all other browsers</a> (e.g. Safari, Samsung Internet) will be to post large buffers via Web Worker <code>postMessage()</code>, or to avoid Web Workers (i.e. perform both physics simulation and rendering on the main thread).</p>
+    <p>Here's a GIF of what it's <em>supposed</em> to look like:</p>
+    <img src="https://birchlabs.co.uk/box2d-wasm-liquidfun/liquidfun.gif" width="350px" height="306">
   {:else}
     <canvas bind:this={canvasElement} width={width} height={height}></canvas>
     <fieldset>
