e41152a2ac749dc32186ecd2aae85d6e833d5296
Alex Birch <Birch-san@users.noreply.github.com>
2021-04-04 18:20:23 +0100

    remove unused

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,5 @@
-const { dirname, resolve } = require('path')
+const { dirname } = require('path')
 const { DefinePlugin } = require('webpack')
-
-/**
- * @param {string} str
- * @returns string
- */
-const escapeRegExp = (str) =>
-  str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
@@ -25,22 +18,6 @@ module.exports = {
         config.plugins.push(new DefinePlugin({
           __SNOWPACK_ENV__: JSON.stringify('production')
         }))
-        config.resolve.alias['box2d-wasm'] = resolve(dirname(require.resolve('box2d-wasm')), '../umd/Box2D.js')
-        // config.output.publicPath = '/box2d-wasm-liquidfun-2/'
-
-        // // /Users/birch/git/liquidfun-play/node_modules/babel-loader/lib/index.js??ruleSet[1].rules[0].use[0]!/Users/birch/git/liquidfun-play/node_modules/@efox/snowpack-plugin-webpack5/plugins/import-meta-fix.js!/Users/birch/git/liquidfun-play/node_modules/@efox/snowpack-plugin-webpack5/plugins/proxy-import-resolve.js!/Users/birch/git/liquidfun-play/build/_snowpack/pkg/box2d-wasm.js
-        // const box2DWasmImportPath = resolve(__dirname, 'build/_snowpack/pkg/box2d-wasm.js')
-        // // config.module.noParse = (resource) => matcher.test(resource)
-        // const matcher = new RegExp(`(!|^)${escapeRegExp(box2DWasmImportPath)}$`)
-        // config.module.noParse = (resource) => {
-        //   const cond = matcher.test(resource)
-        //   console.log(cond, resource)
-        //   return cond
-        // }
-
-        config.optimization.minimizer.pop()
-
-        console.log(config)
         return config
       }
     }]
