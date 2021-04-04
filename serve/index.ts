4afa472ba952f7aeb69ce430ee3bac2f67b8f631
Alex Birch <Birch-san@users.noreply.github.com>
2021-04-04 18:10:27 +0100

    static server working

--- serve/index.ts
+++ serve/index.ts
@@ -1,22 +1,16 @@
-import http from 'http'
-import serveStatic from 'serve-static'
-import finalhandler from 'finalhandler'
+import { dirname, resolve } from 'path'
+import express from 'express'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
 
-const setHeaders = (res: http.ServerResponse, path: string, stat: unknown): any => {
-  // if (mime.lookup(path) === 'application/wasm') {
-  if (path.endsWith('.wasm')) {
-    res.setHeader('Content-Type', 'application/wasm')
-  }
-}
+const require = createRequire(import.meta.url)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const serve: serveStatic.RequestHandler<http.ServerResponse> = serveStatic('../../build', {
-  index: 'index.html',
-  setHeaders
-})
+const repoRoot = resolve(__dirname, '..')
 
-const server: http.Server = http.createServer((req, res) =>
-  serve(req, res, () => finalhandler(req, res))
-)
+const app = express()
+app.use(express.static(resolve(repoRoot, 'build')))
+// seems to resolve to UMD but whatever; it's only WASM we're trying to serve from that folder
+app.use('/box2d-wasm', express.static(dirname(require.resolve('box2d-wasm'))))
 
-// Listen
-server.listen(3000)
+app.listen(3000)
