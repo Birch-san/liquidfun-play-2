a3d4ad43ff4d569ae73a299cce343abd672b4018
Alex Birch <Birch-san@users.noreply.github.com>
2021-07-14 20:59:10 +0100

    add non-useful setTimeout implementation in an attempt to get scheduled more frequently

--- src/loop.ts
+++ src/loop.ts
@@ -132,6 +132,65 @@ export interface DoLoopParams {
 //   }
 // }
 
+// export const doLoop = ({
+//   draw,
+//   physics,
+//   onStats
+// }: DoLoopParams): StopLoop => {
+//   let renderHandle: number | undefined
+//   let lastMs: number | undefined
+//   const renderTask = (): void => {
+//     const nowMs = performance.now()
+//     if (lastMs === undefined) {
+//       lastMs = nowMs - frameIntervalMs
+//     }
+//     const elapsedMs = nowMs - lastMs
+//     {
+//       statsState.animationFrame.frameDurationIx = (statsState.animationFrame.frameDurationIx + 1) % statsState.animationFrame.frameDurationsMs.length
+//       statsState.animationFrame.frameDurationsMs[statsState.animationFrame.frameDurationIx] = elapsedMs
+//       const avgFrameDurationMs = statsState.animationFrame.frameDurationsMs.reduce<number>((acc, next) => acc + next, 0) / statsState.physics.frameDurationsMs.length
+//       onStats({
+//         statsType: 'animationFrame',
+//         stats: {
+//           avgFrameDurationMs: avgFrameDurationMs,
+//           avgFrameRate: 1 / avgFrameDurationMs * 1000
+//         }
+//       })
+//     }
+//     lastMs = nowMs
+
+//     const preMeasureMs = performance.now()
+
+//     // animation frames seem to be scheduled not necessarily
+//     // at 60fps, but sometimes 30fps or 20fps.
+//     // be prepared to calculate 3 frames of physics in normal operation.
+//     // any more infrequent than that probably indicates page got backgrounded;
+//     // if we detect a long gap, we shouldn't attempt to catch up.
+//     physics(Math.min(elapsedMs, frameIntervalMs * 3))
+
+//     {
+//       const durationMs = performance.now() - preMeasureMs
+//       statsState.physics.frameDurationIx = (statsState.physics.frameDurationIx + 1) % statsState.physics.frameDurationsMs.length
+//       statsState.physics.frameDurationsMs[statsState.physics.frameDurationIx] = durationMs
+//       const avgFrameDurationMs = statsState.physics.frameDurationsMs.reduce<number>((acc, next) => acc + next, 0) / statsState.physics.frameDurationsMs.length
+//       onStats({
+//         statsType: 'physics',
+//         stats: {
+//           avgFrameDurationMs: avgFrameDurationMs,
+//           avgFrameRate: 1 / avgFrameDurationMs * 1000
+//         }
+//       })
+//     }
+
+//     draw()
+//     renderHandle = setTimeout(renderTask, frameIntervalMs)
+//   }
+//   renderTask()
+//   return (): void => {
+//     clearTimeout(renderHandle)
+//   }
+// }
+
 export const doLoop = ({
   draw,
   physics,
