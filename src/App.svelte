cb942d966b5b37e0db87ee7d45301c5add9690c7
Alex Birch <Birch-san@users.noreply.github.com>
2021-07-25 18:41:23 +0100

    mobile support

--- src/App.svelte
+++ src/App.svelte
@@ -49,7 +49,7 @@
   const updateMousePos = ({ clientX, clientY }: MouseEvent): void => {
     assert(canvasElement)
     const bounds = canvasElement.getBoundingClientRect()
-    const { left, top } = bounds
+    const { left, top, width, height } = bounds
     const physicalX = clientX - left
     const physicalY = clientY - top
     const xPhysicalToLogical = width / canvasElement.width
@@ -114,85 +114,78 @@
 </script>
   
 <style>
-  .middle-col {
-	  margin-left: auto;
-    margin-right: auto;
-    width: 800px;
-  }
   .fatal-error {
     color: darkred;
   }
   .perf-tracer {
     display: inline-block;
-    width: 40%;
+    width: 25em;
   }
 </style>
-  
-<div class="middle-col">
-  {#if fatalError !== undefined}
-    <pre class="fatal-error">{fatalError}</pre>
-    <h3>Sorry, a fatal error occurred.</h3>
-    <p>This experiment relies on a lot of new Web Worker functionality (<a href="https://stackoverflow.com/a/45578811/5257399">ES imports</a> and <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers"><code>requestAnimationFrame</code></a>). As such, it is currently only expected to work in Chrome/Chromium-based browsers. The plan for <a href="https://github.com/Birch-san/box2d-wasm/discussions/24#discussioncomment-540893">Firefox support</a> will be to use <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer"><code>SharedArrayBuffer</code></a>. The plan for <a href="https://caniuse.com/?search=SharedArrayBuffer">all other browsers</a> (e.g. Safari, Samsung Internet) will be to post large buffers via Web Worker <code>postMessage()</code>, or to avoid Web Workers (i.e. perform both physics simulation and rendering on the main thread).</p>
-    <p>Here's a GIF of what it's <em>supposed</em> to look like:</p>
-    <img src="https://birchlabs.co.uk/box2d-wasm-liquidfun/liquidfun.gif" width="350px" height="306">
-  {:else}
-    <pre class="perf-tracer">
-      Physics 
-      Average frame duration (ms):
-      {statsModel.physics.avgFrameDurationMs.toFixed(2)}
-      Achievable frame rate (fps):
-      {Math.floor(statsModel.physics.avgFrameRate)}
-    </pre>
-    <pre class="perf-tracer">
-      AnimationFrame
-      Average schedule wait (ms):
-      {statsModel.animationFrame.avgFrameDurationMs.toFixed(2)}
-      Achievable frame rate (fps):
-      {Math.floor(statsModel.animationFrame.avgFrameRate)}
-    </pre>
-    <canvas
-    bind:this={canvasElement}
-    on:mousedown={handleMouseDown}
-    on:mouseup={handleMouseUp}
-    on:mouseleave={handleMouseUp}
-    on:mouseenter={handleMouseEnter}
-    on:mousemove={handleMouseMove}
-    width={width}
-    height={height}
-    ></canvas>
-    <fieldset>
-      <legend>Demo</legend>
-      <label>
-        <input type=radio bind:group={demo} value={Demo.Ramp} on:change={onChangeDemo}>
-        Ramp
-      </label>
-      <label>
-        <input type=radio bind:group={demo} value={Demo.Gravity} on:change={onChangeDemo}>
-        Gravity
-      </label>
-      <label>
-        <input type=radio bind:group={demo} value={Demo.WaveMachine} on:change={onChangeDemo}>
-        Wave machine
-      </label>
-      <label>
-        <input type=radio bind:group={demo} value={Demo.None} on:change={onChangeDemo}>
-        None
-      </label>
-    </fieldset>
-    <fieldset>
-      <legend>Effect</legend>
-      <label>
-        <input type=radio bind:group={effect} value={Effect.None}>
-        None
-      </label>
-      <label>
-        <input type=radio bind:group={effect} value={Effect.Refraction}>
-        Refraction
-      </label>
-      <label>
-        <input type=radio bind:group={effect} value={Effect.TemporalBlend}>
-        Temporal blend
-      </label>
-    </fieldset>
-  {/if}
-</div>
+
+{#if fatalError !== undefined}
+  <pre class="fatal-error">{fatalError}</pre>
+  <h3>Sorry, a fatal error occurred.</h3>
+  <p>This experiment relies on a lot of new Web Worker functionality (<a href="https://stackoverflow.com/a/45578811/5257399">ES imports</a> and <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers"><code>requestAnimationFrame</code></a>). As such, it is currently only expected to work in Chrome/Chromium-based browsers. The plan for <a href="https://github.com/Birch-san/box2d-wasm/discussions/24#discussioncomment-540893">Firefox support</a> will be to use <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer"><code>SharedArrayBuffer</code></a>. The plan for <a href="https://caniuse.com/?search=SharedArrayBuffer">all other browsers</a> (e.g. Safari, Samsung Internet) will be to post large buffers via Web Worker <code>postMessage()</code>, or to avoid Web Workers (i.e. perform both physics simulation and rendering on the main thread).</p>
+  <p>Here's a GIF of what it's <em>supposed</em> to look like:</p>
+  <img src="https://birchlabs.co.uk/box2d-wasm-liquidfun/liquidfun.gif" width="350px" height="306">
+{:else}
+  <pre class="perf-tracer">
+    Physics 
+    Average frame duration (ms):
+    {statsModel.physics.avgFrameDurationMs.toFixed(2)}
+    Achievable frame rate (fps):
+    {Math.floor(statsModel.physics.avgFrameRate)}
+  </pre>
+  <pre class="perf-tracer">
+    AnimationFrame
+    Average schedule wait (ms):
+    {statsModel.animationFrame.avgFrameDurationMs.toFixed(2)}
+    Achievable frame rate (fps):
+    {Math.floor(statsModel.animationFrame.avgFrameRate)}
+  </pre>
+  <canvas
+  bind:this={canvasElement}
+  on:mousedown={handleMouseDown}
+  on:mouseup={handleMouseUp}
+  on:mouseleave={handleMouseUp}
+  on:mouseenter={handleMouseEnter}
+  on:mousemove={handleMouseMove}
+  width={width}
+  height={height}
+  ></canvas>
+  <fieldset>
+    <legend>Demo</legend>
+    <label>
+      <input type=radio bind:group={demo} value={Demo.Ramp} on:change={onChangeDemo}>
+      Ramp
+    </label>
+    <label>
+      <input type=radio bind:group={demo} value={Demo.Gravity} on:change={onChangeDemo}>
+      Gravity
+    </label>
+    <label>
+      <input type=radio bind:group={demo} value={Demo.WaveMachine} on:change={onChangeDemo}>
+      Wave machine
+    </label>
+    <label>
+      <input type=radio bind:group={demo} value={Demo.None} on:change={onChangeDemo}>
+      None
+    </label>
+  </fieldset>
+  <fieldset>
+    <legend>Effect</legend>
+    <label>
+      <input type=radio bind:group={effect} value={Effect.None}>
+      None
+    </label>
+    <label>
+      <input type=radio bind:group={effect} value={Effect.Refraction}>
+      Refraction
+    </label>
+    <label>
+      <input type=radio bind:group={effect} value={Effect.TemporalBlend}>
+      Temporal blend
+    </label>
+  </fieldset>
+{/if}
