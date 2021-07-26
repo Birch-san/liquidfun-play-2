b951a21a394fe1541f510bf8123eb8445360dbfc
Alex Birch <Birch-san@users.noreply.github.com>
2021-07-26 23:17:22 +0100

    show SIMD support. explain benchmarks.

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,7 @@
   } from './demoSwitcher'
   import type { StatsType, Stats, OnStatsParams } from './loop'
   import { doLoop, statsTypes } from './loop'
+  import { hasSIMD } from './hasSIMD'
 
   const width = 800
   const height = 700
@@ -91,8 +92,6 @@
     updateMousePos(event)
     eventHandlers?.onMouseMove?.(clickPos)
   }
-
-  let fatalError: string | undefined
   
   onMount(() => {
     assert(canvasElement)
@@ -132,88 +131,97 @@
   }
 </style>
 
-{#if fatalError !== undefined}
-  <pre class="fatal-error">{fatalError}</pre>
-  <h3>Sorry, a fatal error occurred.</h3>
-  <p>This experiment relies on a lot of new Web Worker functionality (<a href="https://stackoverflow.com/a/45578811/5257399">ES imports</a> and <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers"><code>requestAnimationFrame</code></a>). As such, it is currently only expected to work in Chrome/Chromium-based browsers. The plan for <a href="https://github.com/Birch-san/box2d-wasm/discussions/24#discussioncomment-540893">Firefox support</a> will be to use <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer"><code>SharedArrayBuffer</code></a>. The plan for <a href="https://caniuse.com/?search=SharedArrayBuffer">all other browsers</a> (e.g. Safari, Samsung Internet) will be to post large buffers via Web Worker <code>postMessage()</code>, or to avoid Web Workers (i.e. perform both physics simulation and rendering on the main thread).</p>
-  <p>Here's a GIF of what it's <em>supposed</em> to look like:</p>
-  <img src="https://birchlabs.co.uk/box2d-wasm-liquidfun/liquidfun.gif" width="350px" height="306">
+{#if hasSIMD}
+<div><small>Your browser supports <a href="https://v8.dev/features/simd">WebAssembly SIMD</a>, so we will use it.</small></div>
 {:else}
-  <pre class="perf-tracer">
-    Physics 
-    Average frame duration (ms):
-    {statsModel.physics.avgFrameDurationMs.toFixed(2)}
-    Achievable frame rate (fps):
-    {Math.floor(statsModel.physics.avgFrameRate)}
-  </pre>
-  <pre class="perf-tracer">
-    AnimationFrame
-    Average schedule wait (ms):
-    {statsModel.animationFrame.avgFrameDurationMs.toFixed(2)}
-    Achievable frame rate (fps):
-    {Math.floor(statsModel.animationFrame.avgFrameRate)}
-  </pre>
-  <canvas
-  bind:this={canvasElement}
-  on:pointerdown={handleMouseDown}
-  on:pointerup={handleMouseUp}
-  on:pointerleave={handleMouseUp}
-  on:pointerenter={handleMouseEnter}
-  on:pointermove={handleMouseMove}
-  width={width}
-  height={height}
-  ></canvas>
-  <fieldset>
-    <legend>Demo</legend>
-    <label title="Basic use of Box2D without any liquidfun-specific features">
-      <input type=radio bind:group={demo} value={Demo.Ramp} on:change={onChangeDemo}>
-      Ramp
-    </label>
-    <label>
-      <input type=radio bind:group={demo} value={Demo.Gravity} on:change={onChangeDemo}>
-      Gravity
-    </label>
-    <label>
-      <input type=radio bind:group={demo} value={Demo.WaveMachine} on:change={onChangeDemo}>
-      Wave machine
-    </label>
-    <!-- <label title="In case your computer gets hot!">
-      <input type=radio bind:group={demo} value={Demo.None} on:change={onChangeDemo}>
-      None
-    </label> -->
-  </fieldset>
-  {#if demo === Demo.Gravity}
-  <div><small>A small planet is attached to your mouse, and attracts the water.</small></div>
-  {/if}
-  {#if demo === Demo.WaveMachine}
-  <div><small>Click and drag with your mouse to push the water around.</small></div>
-  <fieldset>
-    <legend>Gravity</legend>
-    <label>
-      <input type=radio bind:group={waveMachineGravity} value={WaveMachineGravity.Down} on:change={onChangeWaveMachineGravity}>
-      Down
-    </label>
-    <label>
-      <input type=radio bind:group={waveMachineGravity} value={WaveMachineGravity.None} on:change={onChangeWaveMachineGravity}>
-      None
-    </label>
-  </fieldset>
-  {/if}
-  {#if [Demo.Gravity, Demo.WaveMachine].includes(demo) }
-  <fieldset>
-    <legend>Particle Shader</legend>
-    <label>
-      <input type=radio bind:group={effect} value={Effect.None}>
-      Simple
-    </label>
-    <label>
-      <input type=radio bind:group={effect} value={Effect.Refraction}>
-      Refraction
-    </label>
-    <label>
-      <input type=radio bind:group={effect} value={Effect.TemporalBlend}>
-      Temporal blend
-    </label>
-  </fieldset>
-  {/if}
+<div><small>Your browser does not support a <a href="https://v8.dev/features/simd">WebAssembly SIMD</a>; falling back to standard WebAssembly featureset.</small></div>
+{/if}
+<dl>
+  <dt><small>'Physics' speed:</small></dt>
+  <dd><small>time taken to run one timestep of the physics simulation</small></dd>
+  <dt><small>'AnimationFrame' speed:</small></dt>
+  <dd><small>how frequently the browser repaints</small></dd>
+</dl>
+<p>
+<small>On-CPU time taken to render the game world is neglible (~0.5ms with simple shader).<br>
+Usually the bottleneck is AnimationFrame scheduling; we can simulate physics at a higher framerate, but browser does not want to paint any more frequently.<br>
+"Achievable framerate" is an extrapolation that does not consider realities like CPU temperature.
+</small>
+</p>
+<pre class="perf-tracer">
+  Physics 
+  Average frame duration (ms):
+  {statsModel.physics.avgFrameDurationMs.toFixed(2)}
+  Achievable frame rate (fps):
+  {Math.floor(statsModel.physics.avgFrameRate)}
+</pre>
+<pre class="perf-tracer">
+  AnimationFrame
+  Average schedule wait (ms):
+  {statsModel.animationFrame.avgFrameDurationMs.toFixed(2)}
+  Achievable frame rate (fps):
+  {Math.floor(statsModel.animationFrame.avgFrameRate)}
+</pre>
+<canvas
+bind:this={canvasElement}
+on:pointerdown={handleMouseDown}
+on:pointerup={handleMouseUp}
+on:pointerleave={handleMouseUp}
+on:pointerenter={handleMouseEnter}
+on:pointermove={handleMouseMove}
+width={width}
+height={height}
+></canvas>
+<fieldset>
+  <legend>Demo</legend>
+  <label title="Basic use of Box2D without any liquidfun-specific features">
+    <input type=radio bind:group={demo} value={Demo.Ramp} on:change={onChangeDemo}>
+    Ramp
+  </label>
+  <label>
+    <input type=radio bind:group={demo} value={Demo.Gravity} on:change={onChangeDemo}>
+    Gravity
+  </label>
+  <label>
+    <input type=radio bind:group={demo} value={Demo.WaveMachine} on:change={onChangeDemo}>
+    Wave machine
+  </label>
+  <!-- <label title="In case your computer gets hot!">
+    <input type=radio bind:group={demo} value={Demo.None} on:change={onChangeDemo}>
+    None
+  </label> -->
+</fieldset>
+{#if demo === Demo.Gravity}
+<div><small>A small planet is attached to your mouse, and attracts the water.</small></div>
+{/if}
+{#if demo === Demo.WaveMachine}
+<div><small>Click and drag with your mouse to push the water around.</small></div>
+<fieldset>
+  <legend>Gravity</legend>
+  <label>
+    <input type=radio bind:group={waveMachineGravity} value={WaveMachineGravity.Down} on:change={onChangeWaveMachineGravity}>
+    Down
+  </label>
+  <label>
+    <input type=radio bind:group={waveMachineGravity} value={WaveMachineGravity.None} on:change={onChangeWaveMachineGravity}>
+    None
+  </label>
+</fieldset>
+{/if}
+{#if [Demo.Gravity, Demo.WaveMachine].includes(demo) }
+<fieldset>
+  <legend>Particle Shader</legend>
+  <label>
+    <input type=radio bind:group={effect} value={Effect.None}>
+    Simple
+  </label>
+  <label>
+    <input type=radio bind:group={effect} value={Effect.Refraction}>
+    Refraction
+  </label>
+  <label>
+    <input type=radio bind:group={effect} value={Effect.TemporalBlend}>
+    Temporal blend
+  </label>
+</fieldset>
 {/if}
